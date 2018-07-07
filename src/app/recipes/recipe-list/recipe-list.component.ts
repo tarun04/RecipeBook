d5c1c96232b1c97201800c84b1e91563dab926cf
Tarun Reddy Gaddam <tarunreddy.04@gmail.com>
2018-07-07 17:59:44 -0500

    Added Recipe details outline

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Biryani', 'Hyderabadi', 'https://www.ndtv.com/cooks/images/hyderabadi%20biryani%20new.jpg'),
+    new Recipe('Pizza', 'Italian', 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/pizza-1516720983.jpg?resize=768:*')
+  ];
 
   constructor() { }
 
